1db4fb1b90de30f1dee77075d60f00312b976702
SzymonZebrowski <szymekze@gmail.com>
2019-04-06 01:41:46 +0200

    Corrected models

--- zombathon/toyshare/models.py
+++ zombathon/toyshare/models.py
@@ -12,43 +12,37 @@ class ExtUser(models.Model):
     street = models.CharField(max_length=30)
     house_number = models.CharField(max_length=10)
     post_code = models.CharField(max_length=6)
-    age = models.DecimalField()
+    age = models.DecimalField(max_digits=2, decimal_places=0)
     login = models.CharField(max_length=30)
-    transactions_guantity = models.DecimalField()
+    transactions_quantity = models.DecimalField(max_digits=5, decimal_places=0)
 
 
 class Toy(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=250)
     photo_path = models.TextField(max_length=100)
-    condition = models.DecimalField()
-    age = models.DecimalField()
-    players_quantity = models.DecimalField()
+    condition = models.DecimalField(max_digits=2, decimal_places=0)
+    age = models.DecimalField(max_digits=3, decimal_places=0)
+    players_quantity = models.DecimalField(max_digits=2, decimal_places=0)
     user_id_ref = models.ForeignKey(ExtUser, on_delete=models.CASCADE)
+
 
 class Renting(models.Model):
     begin_date = models.DateField()
     end_date = models.DateField()
-    toy_id_ref = models.ForeignKey(Toy, on_delete=models.PROTECT,
-                                   related_name='toy_id_ref')
-    owner_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT,
-                                    related_name='owner_id_ref')
-    user_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT,
-                                    related_name='user_id_ref')
+    toy_id_ref = models.ForeignKey(Toy, on_delete=models.PROTECT)
+    owner_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT, related_name="owner_id")
+    user_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT, related_name="user_id")
 
 
 class Rate(models.Model):
-    value = models.DecimalField()
+    value = models.DecimalField(max_digits=2, decimal_places=0)
     message = models.TextField(max_length=500)
-    toy_condition = models.DecimalField()
-    renting_id_ref = models.ForeignKey(Renting, on_delete=models.PROTECT,
-                                       related_name='renting_id_ref')
-    user_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT,
-                                    related_name='user_id_ref')
+    toy_condition = models.DecimalField(max_digits=2, decimal_places=0)
+    renting_id_ref = models.ForeignKey(Renting, on_delete=models.PROTECT)
+    user_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT)
 
 
 class Wants(models.Model):
-    user_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT,
-                                    related_name='user_id_ref')
-    toy_id_ref = models.ForeignKey(Toy, on_delete=models.PROTECT,
-related_name='toy_id_ref')
+    user_id_ref = models.ForeignKey(ExtUser, on_delete=models.PROTECT)
+    toy_id_ref = models.ForeignKey(Toy, on_delete=models.PROTECT)
